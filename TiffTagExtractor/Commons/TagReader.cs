ff72e144a083767612b73d7b5764c8f0cae7f57d
pedafou <kompsat9@gmail.com>
2025-04-22 15:15:42 +0900

    Project Initialize

---
+++ TiffTagExtractor/Commons/TagReader.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TiffTagExtractor.Commons
+{
+    class TagReader : BinaryReader
+    {
+        public TagReader(System.IO.Stream stream) : base(stream) { }
+
+        public bool Seek(object offset)
+        {
+            ulong pos = 0;
+            try
+            {
+                pos = Convert.ToUInt64(offset);
+            }
+            catch
+            {
+                return false;
+            }
+
+            if (pos > Int64.MaxValue)
+            {
+                base.BaseStream.Seek((Int64)(Int64.MaxValue), SeekOrigin.Begin);
+                base.BaseStream.Seek((Int64)(pos - Int64.MaxValue), SeekOrigin.Current);
+            }
+            else
+            {
+                base.BaseStream.Seek((Int64)pos, SeekOrigin.Begin);
+            }
+
+            return true;
+        }
+
+        public Array Read(int dataType, int count, bool byteOrder)
+        {
+            switch(dataType)
+            {
+                case 1:
+                    return ReadByte(count, byteOrder);
+                case 2:
+                    return ReadAscii(count, byteOrder);
+                case 3:
+                    return ReadShort(count, byteOrder);
+                case 4:
+                    return ReadLong(count, byteOrder);
+                case 5:
+                    return ReadRational(count, byteOrder);
+                case 6:
+                    return ReadSByte(count, byteOrder);
+                case 7:
+                    return ReadUndefined(count, byteOrder);
+                case 8:
+                    return ReadSShort(count, byteOrder);
+                case 9:
+                    return ReadSLong(count, byteOrder);
+                case 10:
+                    return ReadSRational(count, byteOrder);
+                case 11:
+                    return ReadFloat(count, byteOrder);
+                case 12:
+                    return ReadDouble(count, byteOrder);
+                default:
+                    return null;
+            }
+        }
+
+        // 1 BYTE
+        public byte ReadByte(bool byteOrder)
+        {
+            var data = base.ReadByte();
+            return data;
+        }
+        public byte[] ReadByte(int count, bool byteOrder)
+        {
+            var data = base.ReadBytes(count);
+            return data;
+        }
+
+        // 2 ASCII
+        public char[] ReadAscii(int count, bool byteOrder)
+        {
+            if (count == 0)
+                return new char[] { };
+
+            var data = base.ReadBytes(count);
+            if (byteOrder)
+                Array.Reverse(data);
+
+            return Encoding.ASCII.GetString(data, 0, count).ToCharArray();
+        }
+
+        // 3 Unsigned Short
+        public ushort ReadShort(bool byteOrder)
+        {
+            var data = base.ReadBytes(2);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToUInt16(data, 0);
+        }
+        public ushort[] ReadShort(int count, bool byteOrder)
+        {
+            var result = new ushort[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadShort(byteOrder);
+            return result;
+        }
+
+        // 4 Unsigned LONG
+        public UInt32 ReadLong(bool byteOrder)
+        {
+            var data = base.ReadBytes(4);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
+        }
+        public UInt32[] ReadLong(int count, bool byteOrder)
+        {
+            var result = new UInt32[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadLong(byteOrder);
+            return result;
+        }
+
+        // 5 Unsigned Rational
+        public double ReadRational(bool byteOrder)
+        {
+            var data1 = base.ReadBytes(4);
+            var data2 = base.ReadBytes(4);
+            if (byteOrder)
+            {
+                Array.Reverse(data1);
+                Array.Reverse(data2);
+            }
+
+            return Convert.ToDouble(BitConverter.ToUInt32(data1, 0))
+                / Convert.ToDouble(BitConverter.ToUInt32(data2, 0));
+        }
+        public double[] ReadRational(int count, bool byteOrder)
+        {
+            var result = new double[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadRational(byteOrder);
+            return result;
+        }
+
+        // 6 Signed BYTE
+        public sbyte ReadSByte(bool byteOrder)
+        {
+            var data = base.ReadByte();
+            return Convert.ToSByte(data);
+        }
+        public sbyte[] ReadSByte(int count, bool byteOrder)
+        {
+            var result = new sbyte[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadSByte(byteOrder);
+            return result;
+        }
+
+        // 7 UNDEFINED
+        public byte[] ReadUndefined(int count, bool byteOrder)
+        {
+            var data = base.ReadBytes(count);
+            if (byteOrder)
+                Array.Reverse(data);
+            return data;
+        }
+
+        // 8 Signed SHORT
+        public short ReadSShort(bool byteOrder)
+        {
+            var data = base.ReadBytes(2);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToInt16(data, 0);
+        }
+        public short[] ReadSShort(int count, bool byteOrder)
+        {
+            var result = new short[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadSShort(byteOrder);
+            return result;
+        }
+
+        // 9 Signed LONG
+        public Int32 ReadSLong(bool byteOrder)
+        {
+            var data = base.ReadBytes(4);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToInt32(data, 0);
+        }
+        public Int32[] ReadSLong(int count, bool byteOrder)
+        {
+            var result = new Int32[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadSLong(byteOrder);
+            return result;
+        }
+
+        // 10 Signed Rational
+        public double ReadSRational(bool byteOrder)
+        {
+            var data1 = base.ReadBytes(4);
+            var data2 = base.ReadBytes(4);
+            if (byteOrder)
+            {
+                Array.Reverse(data1);
+                Array.Reverse(data2);
+            }
+
+            return Convert.ToDouble(BitConverter.ToInt32(data1, 0))
+                / Convert.ToDouble(BitConverter.ToInt32(data2, 0));
+        }
+        public double[] ReadSRational(int count, bool byteOrder)
+        {
+            var result = new double[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadSRational(byteOrder);
+            return result;
+        }
+
+        // 11 Single Float
+        public float ReadFloat(bool byteOrder)
+        {
+            var data = base.ReadBytes(4);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToSingle(data, 0);
+        }
+        public float[] ReadFloat(int count, bool byteOrder)
+        {
+            var result = new float[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadFloat(byteOrder);
+            return result;
+        }
+
+        // 12 Double Float
+        public double ReadDouble(bool byteOrder)
+        {
+            var data = base.ReadBytes(8);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToDouble(data, 0);
+        }
+        public double[] ReadDouble(int count, bool byteOrder)
+        {
+            var result = new double[count];
+            for (int i = 0; i < count; i++)
+                result[i] = ReadDouble(byteOrder);
+            return result;
+        }
+
+        // ** Unsigned unsigned 64 Long
+        public UInt64 ReadLong64(bool byteOrder)
+        {
+            var data = base.ReadBytes(8);
+            if (byteOrder)
+                Array.Reverse(data);
+            return BitConverter.ToUInt64(data, 0);
+        }
+
+        public static int GetBytes(int dataType)
+        {
+            switch (dataType)
+            {
+                case 1:
+                case 2:
+                case 6:
+                case 7:
+                    return 1;
+                case 3:
+                case 8:
+                    return 2;
+                case 4:
+                case 9:
+                case 11:
+                    return 4;
+                case 5:
+                case 10:
+                case 12:
+                    return 8;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ TiffTagExtractor/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using TiffTagExtractor.Commons;
+
+namespace TiffTagExtractor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tiffFilePath = string.Empty;
+
+            Console.WriteLine("TIFF TAG Extractor v.1.0.0");
+            Console.WriteLine("==========================");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Insert Path of TIFF File");
+                tiffFilePath = Console.ReadLine();
+            }
+            else
+            {
+                tiffFilePath = args[0];
+            }
+
+            if (!File.Exists(tiffFilePath))
+            {
+                Console.WriteLine("Input File is not Exist");
+                return;
+            }
+
+            ReadTiffTag(tiffFilePath);
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void ReadTiffTag(string fileName, bool genResultFile = false)
+        {
+            Console.WriteLine("\nInput File : {0}", fileName);
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                var tagReader = new TagReader(fs);
+                bool isBigTiff = false;
+                bool byteOrder = false; //isBigEndian : Need Reverse                
+                bool isError = false;
+
+                var byteOrder_1 = tagReader.ReadByte(false);
+                var byteOrder_2 = tagReader.ReadByte(false);
+                var versionNo_1 = tagReader.ReadByte(false);
+                var versionNo_2 = tagReader.ReadByte(false);
+
+                #region ByteOrder / Tiff or BigTiff
+                if (byteOrder_1 == 'I' || byteOrder_2 == 'I')
+                {
+                    byteOrder = false;
+
+                    if (versionNo_1 == 42)
+                        isBigTiff = false;
+                    else if (versionNo_1 == 43)
+                        isBigTiff = true;
+                    else
+                        isError = true;
+                }
+                else if (byteOrder_1 == 'M' || byteOrder_2 == 'M')
+                {
+                    byteOrder = true;
+
+                    if (versionNo_2 == 42)
+                        isBigTiff = false;
+                    else if (versionNo_2 == 43)
+                        isBigTiff = true;
+                    else
+                        isError = true;
+                }
+                else
+                    isError = true;
+
+                if (isError)
+                {
+                    Console.Write("[Error] Input file is not supported ");
+                    return;
+                }
+
+                Console.WriteLine("Type : {0} / {1}", isBigTiff ? "BigTiff" : "Tiff", byteOrder ? "Big Endian" : "Little Endian");
+                #endregion
+
+                #region Define Tag Info (Position / Count / Size)
+
+                ulong startOffset = 0;
+                ulong tagCount = 0;
+
+                var tagSize = isBigTiff ? 20 : 12;
+                var tagOffset = isBigTiff ? 8 : 2;
+                var numberSize = isBigTiff ? 8 : 4;
+                var dataSize = isBigTiff ? 8 : 4;
+
+                if (isBigTiff) // BigTiff
+                {
+                    tagReader.Seek(8);
+                    startOffset = tagReader.ReadLong64(byteOrder);
+                    tagReader.Seek(startOffset);
+                    tagCount = tagReader.ReadLong64(byteOrder);
+                }
+                else // Tiff
+                {
+                    tagReader.Seek(4);
+                    startOffset = tagReader.ReadLong(byteOrder);
+                    tagReader.Seek(startOffset);
+                    tagCount = tagReader.ReadShort(byteOrder);
+                }
+                #endregion
+
+                #region
+                for (int tagIndex = 0; Convert.ToUInt64(tagIndex) < tagCount; tagIndex++)
+                {
+                    tagReader.Seek(startOffset + Convert.ToUInt64(tagOffset + tagIndex * tagSize));
+
+                    var tagID = tagReader.ReadShort(byteOrder);
+                    var tagType = Enum.IsDefined(typeof(TagID), (TagID)tagID) ? (TagID)tagID : TagID.Unknown;
+                    var dataType = tagReader.ReadShort(byteOrder);
+                    var dataBytes = (ulong)TagReader.GetBytes(dataType);
+                    ulong count = isBigTiff ? tagReader.ReadLong64(byteOrder) : tagReader.ReadLong(byteOrder);
+                    if ((dataBytes * count) > Convert.ToUInt64(dataSize))
+                    {
+                        var address = isBigTiff ? tagReader.ReadLong64(byteOrder) : tagReader.ReadLong(byteOrder);
+                        tagReader.Seek(address);
+                    }
+
+                    var isGeoTag = (tagType != TagID.GeoAsciiParamsTag && tagType != TagID.GeoDoubleParamsTag && tagType != TagID.GeoKeyDirectoryTag && tagType != TagID.ModelTransformationTag);
+                    int readCount = isGeoTag && (dataType != 2) && (count > 10) ? 10 : (int)count;
+                    var value = tagReader.Read(dataType, readCount, byteOrder);
+                    Console.Write(" * {0,5} - {1,-30} : ", tagID, tagType);
+                    switch (tagType)
+                    {
+                        case TagID.ModelTransformationTag:
+                        case TagID.GeoKeyDirectoryTag:
+                            Console.WriteLine();
+                            var geoKeyDirectoryTagValue = value.Cast<object>().ToArray();
+                            for (int i = 0; i < (int)count; i += 4)
+                                Console.WriteLine("\t{0}", string.Join("\t", geoKeyDirectoryTagValue.Skip(i).Take(4)));
+                            break;
+                        default:
+                            if (dataType == 2)
+                                Console.Write(string.Join("", value.Cast<object>()));
+                            else
+                                Console.Write(string.Join(", ", value.Cast<object>()));
+
+                            if (readCount < (int)count)
+                                Console.WriteLine("...({0} items)", count);
+                            else
+                                Console.WriteLine();
+
+
+
+                            break;
+                    }
+                }
+                #endregion
+            }
+        }
+    }
+}
